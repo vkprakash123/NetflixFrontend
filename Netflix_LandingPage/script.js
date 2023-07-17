05b2da0eb76360b3db13496b9a707e9c15e08e90
Prakash <krishnaprakash815@gmail.com>
2023-07-17 22:46:23 +0530

    This is old project done in my BTech while learning Web Development

---
+++ Netflix_LandingPage/script.js
@@ -0,0 +1,28 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab content item
+function selectItem (e) {
+    removeBorder();
+    removeShow();
+    // Add border to current tab
+    this.classList.add('tab-border');
+    // Grab Content item from DOM
+    //console.log(this.id);
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    // Add Show class
+
+    tabContentItem.classList.add('show');
+
+}
+
+function removeBorder(){
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow(){
+    tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
+// Listen for tab click
+tabItems.forEach(item => item.addEventListener('click',selectItem));
